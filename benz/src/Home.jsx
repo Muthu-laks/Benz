947c93895bfc33090fe35f5c8dce31edb7bca2bd
Muthulakshmi <123930498+Muthu-laks@users.noreply.github.com>
2024-03-11 23:06:48 +0530

    added sass

--- benz/src/Home.jsx
+++ benz/src/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./Home.css";
 
 export default function index() {
   return (
-    <div>Home</div>
+    <div className="home-page">
+      Home
+      <div className='car'>
+        <div className='car_img'>
+          <img src="car_image.png" />
+        </div>
+      </div>
+    </div>
   )
 }
